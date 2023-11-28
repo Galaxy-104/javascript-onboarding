f5c6df4a14cfff361d14928b8c82155de81ae832
Galaxy <js_park95@naver.com>
2023-11-28 12:24:42 +0900

    feat: 페이지 선택 메서드 수정
    1, 400 페이지 선택 제외 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,15 +1,13 @@
-const pobi = pickRandomPage(1, 400);
-const crong = pickRandomPage(1, 400);
+const pobi = pickRandomPage();
+const crong = pickRandomPage();
 
-function problem1(pobi, crong) {
-  console.log(pobi, crong)
-  var answer;
-  return answer;
-}
+function pickRandomPage(){
+  let page = pickRandomNumber(1, 400)
 
-function pickRandomPage(min, max){
-  let page = Math.floor(Math.random() * (max - min + 1) + min)
-  
+  while(page === 1 || page === 400){
+    page = pickRandomNumber(1, 400)
+  }
+
   if(page % 2 === 1){
     return [page, page + 1]
   }
@@ -17,6 +15,16 @@ function pickRandomPage(min, max){
   if(page % 2 === 0){
     return [page - 1, page]
   }
+}
+
+function pickRandomNumber(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function problem1(pobi, crong) {
+  
+  var answer;
+  return answer;
 }
 
 problem1(pobi, crong);
