dd00126be3162ad37468a4f9f3ab2e192c367338
Galaxy <js_park95@naver.com>
2023-11-28 11:35:06 +0900

    feat: 임의의 페이지 선택 구현
    숫자 랜덤 생성, 페이지 배열로 변환

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,24 @@
+const pobi = pickRandomPage(1, 400);
+const crong = pickRandomPage(1, 400);
+
 function problem1(pobi, crong) {
+  console.log(pobi, crong)
   var answer;
   return answer;
 }
+
+function pickRandomPage(min, max){
+  let page = Math.floor(Math.random() * (max - min + 1) + min)
+  
+  if(page % 2 === 1){
+    return [page, page + 1]
+  }
+
+  if(page % 2 === 0){
+    return [page - 1, page]
+  }
+}
+
+problem1(pobi, crong);
 
 module.exports = problem1;
