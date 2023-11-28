0c9d3ef07cbb739afcc16924ef7c763605f43511
Galaxy <js_park95@naver.com>
2023-11-28 15:38:44 +0900

    feat: 페이지 번호로 값 계산하기
    페이지 번호를 한자리 숫자로 분리, 각 숫자 더하기, 곱하기 구현

--- src/problem1.js
+++ src/problem1.js
@@ -2,28 +2,55 @@ const pobi = pickRandomPage();
 const crong = pickRandomPage();
 
 function pickRandomPage(){
-  let page = pickRandomNumber(1, 400)
+  let page = pickRandomNumber(1, 400);
 
   while(page === 1 || page === 400){
-    page = pickRandomNumber(1, 400)
+    page = pickRandomNumber(1, 400);
   }
 
   if(page % 2 === 1){
-    return [page, page + 1]
+    return [page, page + 1];
   }
 
   if(page % 2 === 0){
-    return [page - 1, page]
+    return [page - 1, page];
   }
 }
 
 function pickRandomNumber(min, max){
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function problem1(pobi, crong) {
-  
+function calculate(number){
+  const numbers = String(number).split('');
+  let sumNumber = 0;
+  let multiNumber = 1;
+
+  numbers.forEach((num) => {
+    sumNumber = sumNumber + Number(num);
+    multiNumber = multiNumber * num;
+  })
+
+  return [sumNumber, multiNumber];
+
+}
+
+function selectMaxNumber(array){
+  const leftPage = calculate(array[0]);
+  const rightPage = calculate(array[1]);
+
+  return Math.max(...leftPage, ...rightPage);
+}
+
+
+
+function problem1(pobi, crong) { 
+
+  const pobiNumber = selectMaxNumber(pobi);
+  const crongNumber = selectMaxNumber(crong);
+
   var answer;
+
   return answer;
 }
 
