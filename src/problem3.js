59bd770df289930cba4342461c08ad8499c2d958
Galaxy <js_park95@naver.com>
2023-11-28 17:51:42 +0900

    feat: 문제3 구현 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,22 @@
+
+function countClapping(number){
+  let index = 1;
+  let count = 0;
+
+  while(index <= number){
+    const numbers = String(index).split('');
+    numbers.forEach((num) => {
+      if(Number(num) === 3 || Number(num) === 6 || Number(num) === 9) count++;
+    })
+    index++
+  }
+
+  return count
+
+}
+
 function problem3(number) {
-  var answer;
+  var answer = countClapping(number);
   return answer;
 }
 
