69ee4b733872d41fcada2d65e7a88e42eb312cde
Galaxy <js_park95@naver.com>
2023-11-28 15:50:05 +0900

    feat: 문제1 기능 구현 완료

--- src/problem1.js
+++ src/problem1.js
@@ -42,22 +42,34 @@ function selectMaxNumber(array){
   return Math.max(...leftPage, ...rightPage);
 }
 
+function validate(pobi, crong){
+
+  if(pobi.length !== 2 || crong.length !== 2) return false
+
+  if(pobi[0] !== pobi[1] - 1 || crong[0] !== crong[1] - 1) return false
+
+  const test = [...pobi, ...crong]
+  
+  test.forEach((number) => {
+    if(isNaN(number)) return false
+    if(number < 1 || number > 400) return false
+  })
+
+  return true
+}
+
 function compareNumber(pobi, crong){
 
-  if(pobi > crong){
-    return 1
-  }
+  if(pobi > crong) return 1
 
-  if(pobi < crong){
-    return 2
-  }
-
-  if(pobi === crong){
-    return 0
-  }
+  if(pobi < crong) return 2
+  
+  if(pobi === crong) return 0
 }
 
 function problem1(pobi, crong) { 
+
+  if(!validate(pobi, crong)) return -1
 
   const pobiNumber = selectMaxNumber(pobi);
   const crongNumber = selectMaxNumber(crong);
