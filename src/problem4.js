46e7c2e4eac8e5fcd7b0ed6738db7679ae3a4873
Galaxy <js_park95@naver.com>
2023-11-29 12:39:50 +0900

    feat: 문제4 기능 구현 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,31 @@
+const upperCase =  Array.from({ length: 26 }, (v, i) => String.fromCharCode(i + 65));
+const lowerCase =  Array.from({ length: 26 }, (v, i) => String.fromCharCode(i + 97));
+
+function changeWord(word){
+  const words = word.split('')
+  const change = words.map((letter) => {
+    if(upperCase.includes(letter)){
+      const array = [...upperCase].reverse();
+      return array[upperCase.indexOf(letter)];
+    }
+
+    if(lowerCase.includes(letter)){
+      const array = [...lowerCase].reverse();
+      return array[lowerCase.indexOf(letter)];
+    }
+
+    return letter
+
+  })
+
+  return change.join('')
+}
+
 function problem4(word) {
-  var answer;
+  var answer = changeWord(word);
   return answer;
 }
+
+problem4("I love you");
 
 module.exports = problem4;
