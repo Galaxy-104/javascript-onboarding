8456e3852a8c4a29c7b0cf27a43ca66807550903
Galaxy <js_park95@naver.com>
2023-11-29 18:09:39 +0900

    feat: 문제2 암호 푸는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,27 @@
+function deleteDuplicete(cryptogram){
+  const words = cryptogram.split('');
+  let isDuplicate = false;
+  let result = words;
+
+  while(!isDuplicate){
+    const filterWord = result.filter((letter, index) => {
+      return letter !== result[index + 1] && letter !== result[index - 1];
+    })
+
+    if(result.join('') === filterWord.join('')) isDuplicate = true;
+
+    result = filterWord;
+
+  }
+
+  return result.join('');
+}
+
 function problem2(cryptogram) {
-  var answer;
+  var answer = deleteDuplicete(cryptogram);
   return answer;
 }
+
+problem2("browoanoommnaon");
 
 module.exports = problem2;
