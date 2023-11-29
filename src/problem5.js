96f63295c0c47ad9da17b35276d202792014405f
Galaxy <js_park95@naver.com>
2023-11-29 14:17:53 +0900

    feat: 문제5 구현 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,30 @@
+const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+function exchangeMoney(money){
+  const result = [];
+  let changeMoney = money
+
+  for(let number of unit){
+    let count = 0;
+    let division = Math.floor(changeMoney / number);
+
+    if(division >= 1){
+      count = division;
+      changeMoney = changeMoney - (number * division);
+    }
+
+    result.push(count);
+  }
+
+  return result;
+
+}
+
 function problem5(money) {
-  var answer;
+  var answer = exchangeMoney(money);
   return answer;
 }
+
+problem5(50237)
 
 module.exports = problem5;
