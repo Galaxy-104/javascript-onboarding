293a13810cbae8d6917a6958fe320c2da55dbad4
Galaxy <js_park95@naver.com>
2023-11-30 11:15:34 +0900

    feat: 문제6 제한사항 확인 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -3,7 +3,7 @@ const users = [ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"],
 
 function findDuplicate(forms){
   const userNames = forms.map((user) => user[1]);
-  const userEmails = forms.map((user) => user[0])
+  const userEmails = forms.map((user) => user[0]);
   const duplicateNames = [];
 
   userNames.forEach((name, nameIndex) => {
@@ -25,9 +25,34 @@ function findDuplicate(forms){
   return result.sort();
 }
 
+function validateForms(forms){
+
+  if(forms.length < 1 || forms.length > 10000){
+    throw new Error('유효하지 않은 데이터입니다.');
+  }
+
+  forms.forEach((user) => {
+    if(user[0].length < 11 || user[0].length >= 20){
+      throw new Error('유효하지 않은 사용자입니다.');
+    }
+
+    const regExp = new RegExp(/[가-힣]/)
+    if(!regExp.test(user[1])){
+      throw new Error('유효하지 않은 사용자입니다.');
+    }
+
+    if(user[1].length < 1 || user[1].length >= 20){
+      throw new Error('유효하지 않은 사용자입니다.')
+    }
+
+  })
+
+}
+
 function problem6(forms) {
+  validateForms(forms);
+
   var answer = findDuplicate(forms);
-  console.log(answer)
   return answer;
 }
 
