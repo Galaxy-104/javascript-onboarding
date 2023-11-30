665140edbaee84f6909e22c17241ef973dffaad2
Galaxy <js_park95@naver.com>
2023-11-30 13:06:36 +0900

    feat: 문제7 사용자의 친구 확인하는 메서드 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,28 @@
+const user = "mrko"
+const friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ]
+const visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"]
+
+function findFriends(user, friends){
+  const userFriends = [];
+  friends.forEach((friend) => {
+    if(friend.includes(user)){
+      userFriends.push(...friend);
+    }
+  })
+
+  return userFriends.filter((friend) => friend !== user)
+}
+
+function recommendFriends(user, friends, visitor){
+  const userFriends = findFriends(user, friends);
+  
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = recommendFriends(user, friends, visitors);
   return answer;
 }
+
+problem7(user, friends, visitors)
 
 module.exports = problem7;
